8ba2066162ca0d08b84990b99939e37bf0afe1bd
Jude-summery <329543240@qq.com>
2019-03-29 11:44:55 +0800

    feat: chunk.js

---
+++ chunk.js
@@ -0,0 +1,13 @@
+function chunk(array, size) {
+    size = Math.max(size, 0);
+    if(array && Object.prototype.toString.call(array) == '[object Array]'){
+        let result = [];
+        for(let i = 0; i < array.length; i+=size){
+            let piece = array.slice(i, i+size);
+            result.push(piece)
+        }
+        return result
+    } else {
+        throw new Error('请传入数组')
+    }
+}
