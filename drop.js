47d27f6d51b2944cdbfd81204b5f2a0f3b0b2a98
Jude-summery <329543240@qq.com>
2019-04-16 11:14:17 +0800

    feat: drop.js

---
+++ drop.js
@@ -0,0 +1,11 @@
+function drop(array, n) {
+    let result = []
+    let resultIndex = -1
+    let number = n == null ? 1 : (n >= 0 ? n : 0)
+    let length = array == null ? 0 : array.length >>> 0
+
+    for (let i = number; i < length; i++) {
+        result[++resultIndex] = array[i]
+    }
+    return result
+}
